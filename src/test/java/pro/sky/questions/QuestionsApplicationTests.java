6f6ff16dd9df90891f364317fff26d765927a9e2
denis <deniz246@mail.ru>
2024-03-26 22:50:02 +0300

    1

---
+++ src/test/java/pro/sky/questions/QuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
